cd555366d5ef9da2e311a659b45b2097f8a2210d
Kong Nguyen <shenlongbk@gmail.com>
2015-08-18 21:58:49 +0800

    add insert sort and selection sort

---
+++ Data Structures & Algorithms/Sort/InsertSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Botter.CodeSnippets.DSA.Sort
+{
+    public class InsertSort
+    {
+        public void Sort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                var insertElement = arr[i];
+
+                // find a place to insert
+                int insertIdx = 0;
+                while (insertIdx < i && arr[insertIdx] < arr[i])
+                    insertIdx++;
+
+                // shift all later elements to the right
+                var j = i;
+                while (j > insertIdx)
+                {
+                    arr[j] = arr[j-1];
+                    j--;
+                }
+                arr[insertIdx] = insertElement;
+            }
+        }
+    }
+}
---
+++ Data Structures & Algorithms/Sort/SelectionSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Botter.CodeSnippets.DSA.Sort
+{
+    class SelectionSort
+    {
+        public void Sort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                // find index of minimum element in unordered list
+                var minIdx = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minIdx])
+                        minIdx = j;
+                }
+
+                // put minimum element to ordered list
+                if (minIdx != i)
+                {
+                    var temp = arr[minIdx];
+                    arr[minIdx] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+    }
+}
