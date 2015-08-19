0e583089d4ab79661844ab586d5e49a9ddfaaee4
dungvt1809 <vutridung.it@gmail.com>
2015-08-19 14:57:42 +0800

    Update LinearSearch.cs

--- Data Structures & Algorithms/Search/LinearSearch.cs
+++ Data Structures & Algorithms/Search/LinearSearch.cs
@@ -4,19 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Botter.CodeSnippets.DSA.Sort
+namespace Botter.CodeSnippets.DSA.Search
 {
     class LinearSearch
     {
         public int Search(int[] arr, int value)
         {
             for (int i = 0; i < arr.Length; i++)
-		    {
+	    {
                 if (arr[i] == value)
                 {
-				    return i;
-			    }
-		    }
+		    return i;
+		}
+	    }
             // value is not exist in array
             return -1;
         }
