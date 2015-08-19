a29380e065b7b867a5f26d773bcde17e750ed6a6
dungvt1809 <vutridung.it@gmail.com>
2015-08-19 14:56:59 +0800

    Update BinarySearch.cs

--- Data Structures & Algorithms/Search/BinarySearch.cs
+++ Data Structures & Algorithms/Search/BinarySearch.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Botter.CodeSnippets.DSA.Sort
+namespace Botter.CodeSnippets.DSA.Search
 {
     class BinarySearch
     {
