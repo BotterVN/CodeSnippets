67458aa114bbc1f2804b11540351d80e33e5c7ca
Kong Nguyen <shenlongbk@gmail.com>
2015-08-20 23:31:42 +0800

    add graph data structure

---
+++ Data Structures & Algorithms/Graph/DirectedGraph.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Botter.CodeSnippets.DSA.Graph
+{
+    public class DirectedGraph<T>
+    {
+        Dictionary<T, HashSet<T>> _edges = new Dictionary<T, HashSet<T>>();
+        List<T> _vertices = new List<T>();
+
+        public void AddVertex(T vertex)
+        {
+            if (_vertices.Contains(vertex))
+                throw new Exception("this element was in graph");
+            else
+                _vertices.Add(vertex);
+        }
+
+        public void AddEdge(T fromVertex, T toVertex)
+        {
+            if (!_vertices.Contains(fromVertex))
+                throw new Exception(string.Format("element {0} doesn't exist in graph", fromVertex));
+
+            if(!_vertices.Contains(toVertex))
+                throw new Exception(string.Format("element {0} doesn't exist in graph", toVertex));
+
+            var descendants = (_edges.ContainsKey(fromVertex)) ? _edges[fromVertex] : (new HashSet<T>());
+            _edges[fromVertex] = descendants;
+
+            descendants.Add(toVertex);
+        }
+
+        public T[] GetVertices()
+        {
+            return _vertices.ToArray();
+        }
+
+        public bool[,] GetEdges()
+        {
+            var edges = new bool[_vertices.Count, _vertices.Count];
+            for (int i = 0; i < _vertices.Count; i++)
+            {
+                for (int j = 0; j < _vertices.Count; j++)
+                {
+                    if (HasEdge(_vertices[i], _vertices[j]))
+                        edges[i, j] = true;
+                    else
+                        edges[i, j] = false;
+                }
+            }
+
+            return edges;
+        }
+
+        public bool HasEdge(T fromVertex, T toVertex)
+        {
+            return (_edges.ContainsKey(fromVertex) && _edges[fromVertex].Contains(toVertex));
+        }
+
+        public T[] GetAllDescendants(T node)
+        {
+            return _edges[node].ToArray();
+        }
+
+        public bool IsReachable(T fromVertex, T toVertex)
+        {
+            bool[] visited = Enumerable.Repeat(false, _vertices.Count).ToArray();
+
+            var vertices = GetVertices();
+            bool[,] edges = GetEdges();
+
+            var fromIdx = Array.IndexOf(vertices, fromVertex);
+            var toIdx = Array.IndexOf(vertices, toVertex);
+
+            var traversingNodes = new Queue<int>();
+            traversingNodes.Enqueue(fromIdx);
+
+            while (traversingNodes.Count > 0)
+            {
+                var curNode = traversingNodes.Dequeue();
+                visited[curNode] = true;
+
+                for (int i = 0; i < vertices.Length; i++)
+                {
+                    if (!visited[i] && edges[curNode, i])
+                    {
+                        if (i == toIdx)
+                            return true;
+                        else
+                            traversingNodes.Enqueue(i);
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Data Structures & Algorithms/Tests/TestGraph.cs
@@ -0,0 +1,37 @@
+﻿using Botter.CodeSnippets.DSA.Graph;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotterDSA.Tests
+{
+    [TestClass]
+    public class TestGraph
+    {
+        [TestMethod]
+        public void TestCreateGraph()
+        {
+            var graph = new DirectedGraph<char>();
+            graph.AddVertex('A');
+            graph.AddVertex('B');
+            graph.AddVertex('D');
+            graph.AddVertex('E');
+            graph.AddEdge('A', 'B');
+            graph.AddEdge('A', 'D');
+            graph.AddEdge('D', 'B');
+            graph.AddEdge('D', 'E');
+            graph.AddEdge('E', 'B');
+
+            Assert.IsTrue(graph.HasEdge('D', 'B'));
+            Assert.IsTrue(graph.HasEdge('E', 'B'));
+            Assert.IsFalse(graph.HasEdge('A', 'E'));
+
+            Assert.IsTrue(graph.IsReachable('A', 'D'));
+            Assert.IsTrue(graph.IsReachable('A', 'E'));
+            Assert.IsFalse(graph.IsReachable('B', 'E'));
+        }
+    }
+}
