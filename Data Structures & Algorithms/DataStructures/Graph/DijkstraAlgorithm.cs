6405111e8e49e6ca7c64f473145afdcd20afc670
Kong Nguyen <shenlongbk@gmail.com>
2015-08-21 17:19:52 +0800

    add dijkstra algorithm

---
+++ Data Structures & Algorithms/DataStructures/Graph/DijkstraAlgorithm.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotterDSA.Graph
+{
+    class Node
+    {
+        public Node()
+        {
+            IsVisited = false;
+            Distance = -1;
+            PrevNode = -1;
+        }
+
+        public bool IsVisited { get; set; }
+        public int Distance { get; set; }
+        public int PrevNode { get; set; }
+    }
+
+    public class DijkstraAlgorithm<T>
+    {
+        T[] _vertices;
+        int[,] _edges;
+
+        public DijkstraAlgorithm(T[] vertices, int[,] edges)
+        {
+            _vertices = vertices;
+            _edges = edges;
+        }
+
+        public T[] FindPath(int fromIdx, int toIdx)
+        {
+            var nodes = new Node[_vertices.Length];
+            for (int i = 0; i < _vertices.Length; i++)
+                nodes[i] = new Node();
+
+            var curNode = fromIdx;
+            nodes[curNode].Distance = 0;
+
+            while(curNode != -1)
+            {
+                nodes[curNode].IsVisited = true;
+
+                for (int i = 0; i < _vertices.Length; i++)
+                {
+                    if(!nodes[i].IsVisited && _edges[curNode, i] != -1)
+                    {
+                        if(i == toIdx)
+                        {
+                            // I found you
+                            var path = new List<T>();
+                            nodes[toIdx].PrevNode = curNode;
+                            var iterNode = toIdx;
+                            while(nodes[iterNode].PrevNode != -1)
+                            {
+                                path.Add(_vertices[iterNode]);
+                                iterNode = nodes[iterNode].PrevNode;
+                            }
+
+                            if (iterNode != fromIdx)
+                                throw new Exception("your algorithm is broken");
+
+                            path.Add(_vertices[fromIdx]);
+                            path.Reverse();
+
+                            return path.ToArray();
+                        }
+                        else if(nodes[i].Distance == -1 || nodes[curNode].Distance + _edges[curNode, i] < nodes[i].Distance)
+                        {
+                            nodes[i].Distance = nodes[curNode].Distance + _edges[curNode, i];
+                            nodes[i].PrevNode = curNode;
+                        }
+                    }
+                }
+
+                curNode = -1;
+                var minDistance = -1;
+                for (int i = 0; i < _vertices.Length; i++)
+                {
+                    if (!nodes[i].IsVisited && nodes[i].Distance > 0 && (curNode == -1 || nodes[i].Distance < minDistance))
+                    {
+                        curNode = i;
+                        minDistance = nodes[i].Distance;
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
--- Data Structures & Algorithms/DataStructures/Tests/TestGraph.cs
+++ Data Structures & Algorithms/DataStructures/Tests/TestGraph.cs
@@ -1,4 +1,5 @@
 ﻿using Botter.CodeSnippets.DSA.Graph;
+using BotterDSA.Graph;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
@@ -50,6 +51,25 @@ namespace BotterDSA.Tests
             var dfs = new DFS<char>(_graph);
             var path2 = dfs.GetPath('A', 'E');
             CollectionAssert.AreEqual(path2, new char[] { 'A', 'D', 'E' });
+        }
+
+        [TestMethod]
+        public void TestDijkstra()
+        {
+            var vertices = new int[] { 1, 2, 3, 4, 5, 6 };
+            var edges = new int[,]
+            {
+                { -1, 7, 9, -1, -1, 14 },
+                { 7, -1, 10, 15, -1, -1 },
+                { 8, 10, -1, 11, -1, 2 },
+                { -1, 15, 11, -1, 6, -1 },
+                { -1, -1, -1, 6, -1, 9 },
+                { 14, -1, 2, -1, 9, -1}
+            };
+
+            var dijkstra = new DijkstraAlgorithm<int>(vertices, edges);
+            var path = dijkstra.FindPath(0, 4);
+            CollectionAssert.AreEqual(path, new int[] { 1, 3, 6, 5 });            
         }
     }
 }
