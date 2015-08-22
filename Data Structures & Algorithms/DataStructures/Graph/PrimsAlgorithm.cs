b6fd4c26f9f6912446e9989afc34c7da07f6ec25
Kong Nguyen <shenlongbk@gmail.com>
2015-08-22 11:45:55 +0800

    add prims algorithm

---
+++ Data Structures & Algorithms/DataStructures/Graph/PrimsAlgorithm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Botter.CodeSnippets.DSA.Graph
+{
+    public class PrimsAlgorithm<T>
+    {
+        T[] _vertices;
+        int[,] _edges;
+
+        public PrimsAlgorithm(T[] vertices, int[,] edges)
+        {
+            _vertices = vertices;
+            _edges = edges;
+        }
+
+        public T[] FindSpanningTree()
+        {
+            var spanningTree = new T[_vertices.Length];
+            var spanningTreeIdx = new int[_vertices.Length];
+            var isVisited = Enumerable.Repeat(false, _vertices.Length).ToArray();
+
+            var curVertexIdx = 0;
+            spanningTreeIdx[curVertexIdx++] = 0;
+            isVisited[0] = true;
+
+            while(curVertexIdx < _vertices.Length)
+            {
+                var minimumNodeIdx = -1;
+                var minDist = Int32.MaxValue;
+
+                for (int i = 0; i < curVertexIdx; i++)
+                {
+                    for (int j = 0; j < _vertices.Length; j++)
+                    {
+                        if (!isVisited[j] && _edges[i, j] != -1 && _edges[i, j] < minDist)
+                        {
+                            minimumNodeIdx = j;
+                            minDist = _edges[i, j];
+                        }
+                    }
+                }
+
+                spanningTreeIdx[curVertexIdx++] = minimumNodeIdx;
+                isVisited[minimumNodeIdx] = true;
+            }
+
+            return spanningTreeIdx.Select(idx => _vertices[idx]).ToArray();
+        }
+    }
+}
--- Data Structures & Algorithms/Tests/DataStructure/TestGraph.cs
+++ Data Structures & Algorithms/Tests/DataStructure/TestGraph.cs
@@ -1,5 +1,4 @@
 ﻿using Botter.CodeSnippets.DSA.Graph;
-using BotterDSA.Graph;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
@@ -13,6 +12,16 @@ namespace BotterDSA.Tests
     public class TestGraph
     {
         DirectedGraph<char> _graph;
+        int[] _vertices = new int[] { 1, 2, 3, 4, 5, 6 };
+        int[,] _edges = new int[,]
+            {
+                { -1, 7, 9, -1, -1, 14 },
+                { 7, -1, 10, 15, -1, -1 },
+                { 8, 10, -1, 11, -1, 2 },
+                { -1, 15, 11, -1, 6, -1 },
+                { -1, -1, -1, 6, -1, 9 },
+                { 14, -1, 2, -1, 9, -1}
+            };
 
         [TestInitialize]
         public void InitGraph()
@@ -56,20 +65,17 @@ namespace BotterDSA.Tests
         [TestMethod]
         public void TestDijkstra()
         {
-            var vertices = new int[] { 1, 2, 3, 4, 5, 6 };
-            var edges = new int[,]
-            {
-                { -1, 7, 9, -1, -1, 14 },
-                { 7, -1, 10, 15, -1, -1 },
-                { 8, 10, -1, 11, -1, 2 },
-                { -1, 15, 11, -1, 6, -1 },
-                { -1, -1, -1, 6, -1, 9 },
-                { 14, -1, 2, -1, 9, -1}
-            };
-
-            var dijkstra = new DijkstraAlgorithm<int>(vertices, edges);
+            var dijkstra = new DijkstraAlgorithm<int>(_vertices, _edges);
             var path = dijkstra.FindPath(0, 4);
             CollectionAssert.AreEqual(path, new int[] { 1, 3, 6, 5 });            
+        }
+
+        [TestMethod]
+        public void TestPrims()
+        {
+            var prims = new PrimsAlgorithm<int>(_vertices, _edges);
+            var spanningTree = prims.FindSpanningTree();
+            CollectionAssert.AreEqual(spanningTree, null);
         }
     }
 }
