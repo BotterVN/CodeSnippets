2707e91194343bd4928f59464c8e1d121a310085
Kong Nguyen <shenlongbk@gmail.com>
2015-08-25 17:09:56 +0800

    boyer moore: add bad character preprocessing

--- Data Structures & Algorithms/DataStructures/Search/BoyerMoore.cs
+++ Data Structures & Algorithms/DataStructures/Search/BoyerMoore.cs
@@ -14,50 +14,30 @@ namespace Botter.CodeSnippets.DSA.Search
             var gs = -1;
 
             // bad character rule
+            var NUM_OF_ASCII_CHARS = (int)(Math.Pow(2, 8) - 1);
+
+            var bcTable = new int[NUM_OF_ASCII_CHARS];
+            for (int n = 0; n < s.Length; n++)
+            {
+                int idx = pattern.Length - 1;
+                while (idx >= 0 && pattern[idx] != s[n])
+                    idx--;
+                bcTable[s[n]] = idx;
+            }
 
             // good suffix rule
 
-            for (int i = 0; i < s.Length; i++)
+            var i = 0;
+            while(i < s.Length-pattern.Length)
             {
-                int j;
-                for (j = pattern.Length-1; j >= 0; j--)
-                {
-                    if(s[i+j] != pattern[j])
-                        break;
-                    else if(j == 0)
-                        return i;
-                }
+                int j = pattern.Length-1;
+                while (j >= 0 && s[i + j] == pattern[j])
+                    j--;
 
-                int k;
-                //
-                var badCharacter = s[i + j];
-                k = j - 1;
-                while (k >= i && pattern[k] != badCharacter)
-                    k--;
+                if (j == -1)
+                    return i;
 
-                bc = i + j - k - 1;
-
-                //
-                // find pattern p[j+1:len]
-                var substringLen = pattern.Length - (j + 1);
-
-                if (substringLen != 0)
-                {
-                    k = i + pattern.Length - 2;
-                    bool isMatch = false;
-                    while (k >= i && !isMatch)
-                    {
-                        isMatch = true;
-                        for (int l = 0; l < substringLen; l++)
-                        {
-                            if (s[j + 1 + l] != pattern[l])
-                                isMatch = false;
-                        }
-                    }
-                    gs = i + j - k - 1;
-                }
-                else
-                    gs = pattern.Length - 1;
+                bc = Math.Max(1, j-bcTable[s[i+j]]);
 
                 i += Math.Max(bc, gs);
             }
