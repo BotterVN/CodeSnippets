63f4b73bf332b45d4932f8f1632bf13d286abe53
Kong Nguyen <shenlongbk@gmail.com>
2015-08-27 19:19:56 +0800

    add avl

---
+++ Data Structures & Algorithms/DataStructures/Tree/AVL.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Botter.CodeSnippets.DSA.Tree
+{
+    public class AVL
+    {
+        public AVL Left { get; set; }
+        public AVL Right { get; set; }
+        public int Value { get; set; }
+
+        public AVL()
+        {
+            Left = null;
+            Right = null;
+        }
+
+        public void InsertWithAutoBalance(int val)
+        {
+            Insert(val);
+        }
+
+        public void Insert(int val)
+        {
+            if (val > Value)
+            {
+                if (Right == null)
+                {
+                    var newNode = new AVL() { Value = val };
+                    Right = newNode;
+                }
+                else
+                    Right.Insert(val);
+            }
+            else
+            {
+                if(Left == null)
+                {
+                    var newNode = new AVL() { Value = val };
+                    Left = newNode;
+                }
+                else
+                    Left.Insert(val);
+            }
+        }
+
+        private void RotateLeft()
+        {
+
+        }
+
+        public void Print()
+        {
+
+        }
+    }
+}
