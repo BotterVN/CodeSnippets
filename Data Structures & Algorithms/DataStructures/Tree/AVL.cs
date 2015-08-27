dea3e19dd660facaf738e05314aaabde2b4739ce
Kong Nguyen <shenlongbk@gmail.com>
2015-08-28 00:07:32 +0800

    update avl

--- Data Structures & Algorithms/DataStructures/Tree/AVL.cs
+++ Data Structures & Algorithms/DataStructures/Tree/AVL.cs
@@ -20,10 +20,15 @@ namespace Botter.CodeSnippets.DSA.Tree
 
         public void InsertWithAutoBalance(int val)
         {
-            Insert(val);
+            Insert(val, null);
         }
 
-        public void Insert(int val)
+        public AVL Insert(int val)
+        {
+            return Insert(val, null);
+        }
+
+        private AVL Insert(int val, AVL parent)
         {
             if (val > Value)
             {
@@ -33,7 +38,7 @@ namespace Botter.CodeSnippets.DSA.Tree
                     Right = newNode;
                 }
                 else
-                    Right.Insert(val);
+                    Right.Insert(val, this);
             }
             else
             {
@@ -43,18 +48,114 @@ namespace Botter.CodeSnippets.DSA.Tree
                     Left = newNode;
                 }
                 else
-                    Left.Insert(val);
+                    Left.Insert(val, this);
             }
+            return BalanceTree(parent);
         }
 
-        private void RotateLeft()
+        private AVL BalanceTree(AVL parent)
         {
+            var bf = GetBalanceFactor();
+            if(bf == 2)
+            {
+                var subBf = this.Left.GetBalanceFactor();
+                if(subBf == -1) // left-right
+                    this.Left.RotateLeft(this);
+                 
+                // left-left
+                return this.RotateRight(parent);
+            }
+            else if(bf == -2)
+            {
+                var subBf = this.Right.GetBalanceFactor();
+                if(subBf == 1) // right-left
+                    this.Right.RotateRight(this);
 
+                return this.RotateLeft(parent);
+            }
+            else
+                return this;
+        }
+
+        public int GetBalanceFactor()
+        {
+            return Height(this.Left) - Height(this.Right);
+        }
+
+        public int Height()
+        {
+            return Height(this);
+        }
+
+        public int Height(AVL tree)
+        {
+            if (tree == null)
+                return 0;
+
+            return 1 + Math.Max(Height(tree.Left), Height(tree.Right));
+        }
+
+        private AVL RotateLeft(AVL parent)
+        {
+            var right = this.Right;
+
+            this.Right = right.Left;
+            right.Left = this;
+
+            if(parent != null)
+                parent.Left = right;
+
+            return right;
+        }
+
+        private AVL RotateRight(AVL parent)
+        {
+            var left = this.Left;
+
+            this.Left = left.Right;
+            left.Right = this;
+
+            if(parent != null)
+                parent.Right = left;
+
+            return left;
         }
 
         public void Print()
         {
+            Console.WriteLine("@@@@@@@@@@");
+            Print(0);
+        }
 
+        private void Print(int level)
+        {
+            PrintMultiple(" ", level * 3);
+            Console.WriteLine("+- {0}", Value);
+
+            if (Left != null)
+                Left.Print(level + 1);
+
+            if (Right != null)
+                Right.Print(level + 1);
+            
+        }
+
+        public void PrintMultiple(string s, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(s);
+            }
+        }
+
+        public int NumChildren()
+        {
+            return NumChildren(this);
+        }
+
+        public int NumChildren(AVL tree)
+        {
+            return 1 + NumChildren(tree.Left) + NumChildren(tree.Right);
         }
     }
 }
